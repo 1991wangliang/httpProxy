1625ce1f766decd3b73709bcf7abc4a5086b7c69
wangliang <1991wangliang@gmail.com>
2017-09-03 09:08:23 +0800

    http proxy

--- src/main/java/com/lorne/http/service/impl/HttpServerImpl.java
+++ src/main/java/com/lorne/http/service/impl/HttpServerImpl.java
@@ -3,23 +3,14 @@ package com.lorne.http.service.impl;
 import com.lorne.http.service.HttpServer;
 import com.lorne.http.service.UserService;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
 import io.netty.handler.codec.http.HttpRequest;
-import net.lightbody.bmp.mitm.CertificateAndKeySource;
-import net.lightbody.bmp.mitm.KeyStoreFileCertificateSource;
-import net.lightbody.bmp.mitm.PemFileCertificateSource;
-import net.lightbody.bmp.mitm.RootCertificateGenerator;
-import net.lightbody.bmp.mitm.keys.ECKeyGenerator;
-import net.lightbody.bmp.mitm.manager.ImpersonatingMitmManager;
-import org.littleshoot.proxy.HttpFilters;
-import org.littleshoot.proxy.HttpFiltersSourceAdapter;
-import org.littleshoot.proxy.HttpProxyServer;
-import org.littleshoot.proxy.HttpProxyServerBootstrap;
+import org.littleshoot.proxy.*;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.net.InetSocketAddress;
 
 /**
@@ -31,55 +22,34 @@ public class HttpServerImpl implements HttpServer {
     @Value("${http.port}")
     private int httpPort;
 
-    private HttpProxyServer server = null;
+    private  HttpProxyServer server = null;
 
     @Autowired
     private UserService userService;
 
-
-
-
-
     @Override
     public void start() {
+        server = DefaultHttpProxyServer.bootstrap()
+                .withProxyAuthenticator(new ProxyAuthenticator() {
 
-        //http://blog.csdn.net/houjixin/article/details/25806151
-        //https://github.com/lightbody/browsermob-proxy/tree/master/mitm
-        String path = "d://lcn";
-        String cerPath = path+"/myca.cer";
-        String pemPath = path+"/mykey.pem";
-        String password = "pwdm12345x";
+                    @Override
+                    public boolean authenticate(String userName, String password) {
+                        return userService.authenticate(userName,password);
+                    }
 
-        // create a dyamic CA root certificate generator using Elliptic Curve keys
-        RootCertificateGenerator ecRootCertificateGenerator = RootCertificateGenerator.builder()
-                .keyGenerator(new ECKeyGenerator())     // use EC keys, instead of the default RSA
-                .build();
-
-        // save the dynamically-generated CA root certificate for installation in a browser
-        ecRootCertificateGenerator.saveRootCertificateAsPemFile(new File(cerPath));
-
-        // save the dynamically-generated CA private key for use in future LittleProxy executions
-        // (see CustomCAPemFileExample.java for an example loading a previously-generated CA cert + key from a PEM file)
-        ecRootCertificateGenerator.savePrivateKeyAsPemFile(new File(pemPath),
-                password);
-
-
-        PemFileCertificateSource fileCertificateSource = new PemFileCertificateSource(
-                new File(cerPath),    // the PEM-encoded certificate file
-                new File(pemPath),    // the PEM-encoded private key file
-                password);
-
-        ImpersonatingMitmManager mitmManager = ImpersonatingMitmManager.builder()
-                .rootCertificateSource(fileCertificateSource)
-             //   .serverKeyGenerator(new ECKeyGenerator())
-                .build();
-
-        HttpProxyServerBootstrap bootstrap = DefaultHttpProxyServer.bootstrap()
-                .withManInTheMiddle(mitmManager)
-                .withAddress(new InetSocketAddress(httpPort));
-
-        server = bootstrap.start();
-
+                    @Override
+                    public String getRealm() {
+                        return "admin";
+                    }
+                })
+                .withFiltersSource(new HttpFiltersSourceAdapter(){
+                    @Override
+                    public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
+                        return super.filterRequest(originalRequest, ctx);
+                    }
+                })
+                .withAddress(new InetSocketAddress(httpPort))
+                .start();
 
     }
 
